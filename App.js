4326302c0bc62eabac47897f7d78367f33e7cf25
Eyitayo Itunu Babatope <112722909+eyitayoit-alt@users.noreply.github.com>
2023-08-06 09:28:25 +0100

    Create App.js

---
+++ App.js
@@ -0,0 +1,21 @@
+
+import React from 'react';
+import "./app.css";
+import TogglePassword from  './UseRef';
+
+
+const App = () => {
+ 
+ 
+  return (
+    <div className='App'>
+    
+    
+        <TogglePassword  />
+      
+      
+    </div>
+  )
+}
+
+export default App
